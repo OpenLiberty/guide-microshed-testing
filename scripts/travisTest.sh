24166dc46ee71e9b9620140d53b958096a630874
Gilbert Kwan <gkwan@ca.ibm.com>
2019-11-04 13:35:15 -0500

    Add test

---
+++ scripts/travisTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+##############################################################################
+##
+##  Travis CI test script
+##
+##############################################################################
+
+mvn -q clean verify
+mvn -q clean package liberty:create liberty:install-feature liberty:deploy
+mvn liberty:start
+curl http://localhost:9080/guide-microshed-testing/people
+mvn liberty:stop
