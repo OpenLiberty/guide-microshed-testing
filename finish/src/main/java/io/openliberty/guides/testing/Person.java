05fddf4f858e9f52b503a20bcd71d5668ef526f7
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-24 14:02:32 -0500

    linter fix

--- finish/src/main/java/io/openliberty/guides/testing/Person.java
+++ finish/src/main/java/io/openliberty/guides/testing/Person.java
@@ -1,7 +1,13 @@
 // tag::copyright[]
 /*******************************************************************************
  * Copyright (c) 2019, 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
  *
